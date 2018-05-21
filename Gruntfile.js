c700d34eb7b7e2b2fdec6f77c465436e838a9351
anthkris <anthkris09@gmail.com>
2018-05-21 12:48:47 -0500

    chore: Add grunt and package.json
    Add grunt config to make new responsive images.
    Add grunt and dependencies to package.json.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: "gm",
+          sizes: [{
+            width: 800,
+            suffix: "_large",
+            quality: 50
+          },
+          {
+            width: 600,
+            suffix: "_medium",
+            quality: 50
+          },
+          {
+            width: 300,
+            suffix: "_small",
+            quality: 50
+          }]
+        },
+
+        files: [{
+          expand: true,
+          src: ["*.{gif,jpg,png}"],
+          cwd: "img/",
+          dest: "images_responsive/"
+        }]
+      }
+    },
+    clean: {
+      dev: {
+        src: ["images_responsive/"],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ["images"]
+        },
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks("grunt-responsive-images");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-mkdir");
+  grunt.registerTask("default", ["clean", "mkdir", "responsive_images"]);
+
+};
